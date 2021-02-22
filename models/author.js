d38216d59dea78e56efa67866793de2fe834e462
TranHoNhiThanh <nhithanhtranho@gmail.com>
2021-02-22 16:32:30 +0700

    initModel

--- models/author.js
+++ models/author.js
@@ -5,36 +5,39 @@ import {
 
 class Author extends Model {}
 
-Author.init({
-    id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-        fields: "id"
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        field: "name"
-    },
-    nationality: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        field: "nationality"
-    },
-    gender: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: true,
-        field: "gender"
-    },
-    biography: {
-        type: Sequelize.STRING,
-        defaultValue: "No biography describe!",
-        field: "biography"
-    }
-}, {
-    tableName: "author",
-    timestamps: true,
-    paranoid: true
-})
+export function initAuthorModel(sequelize) {
+    Author.init({
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            fields: "id"
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            field: "name"
+        },
+        nationality: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            field: "nationality"
+        },
+        gender: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: true,
+            field: "gender"
+        },
+        biography: {
+            type: Sequelize.STRING,
+            defaultValue: "No biography describe!",
+            field: "biography"
+        }
+    }, {
+        sequelize,
+        tableName: "author",
+        timestamps: true,
+        paranoid: true
+    })
+}
--- models/book.js
+++ models/book.js
@@ -1,22 +1,29 @@
-import {Sequelize, Model} from "sequelize"
+import {
+  Sequelize,
+  Model
+} from "sequelize"
 
 export class Book extends Model {}
 
-Book.init({
-  id: {
-    type: Sequelize.UUID,
-    default: Sequelize.UUIDV4,
-    primaryKey: true,
-    field: "id"
-  },
-  name: {
+export function initBookModel(sequelize) {
+
+  Book.init({
+    id: {
+      type: Sequelize.UUID,
+      default: Sequelize.UUIDV4,
+      primaryKey: true,
+      field: "id"
+    },
+    name: {
       type: Sequelize.String,
       unique: true,
       allowNull: false,
       field: "name"
-  }
-}, {
-  tableName: "book",
-  timestamps: true,
-  paranoid: true
-})
+    }
+  }, {
+    sequelize,
+    tableName: "book",
+    timestamps: true,
+    paranoid: true
+  })
+}
